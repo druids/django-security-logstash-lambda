6a610702eebbf3712d632d90d2a913d95e35d7e3
Luboš Mátl <matllubos@gmail.com>
2022-03-01 19:09:44 +0100

    Fixed minor bugs

--- lambda_function.py
+++ lambda_function.py
@@ -9,12 +9,10 @@ try:
 except ImportError:
     import simplejson as json
 
-
 # Parameters
 host = os.environ['LOGSTASH_HOST']
 port = int(os.environ['LOGSTASH_PORT'])
-
-metadata = {}
+timeout = int(os.environ.get('LOGSTASH_TIMEOUT', 5))
 
 
 def serialize_message(message, metadata):
@@ -34,49 +32,44 @@ def lambda_handler(event, context):
         )
 
     # Attach Logstash TCP Socket
+    socket.setdefaulttimeout(timeout)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
 
     try:
         # Add the context to meta
-        metadata['aws'] = {}
-        metadata['aws']['function_name'] = context.function_name
-        metadata['aws']['function_version'] = context.function_version
-        metadata['aws']['invoked_function_arn'] = context.invoked_function_arn
-        metadata['aws']['memory_limit_in_mb'] = context.memory_limit_in_mb
-
+        metadata = {
+            'aws': {
+                'function_name': context.function_name,
+                'function_version': context.function_version,
+                'invoked_function_arn': context.invoked_function_arn,
+                'memory_limit_in_mb': context.memory_limit_in_mb,
+            }
+        }
         # Parse logs
         logs = awslogs_handler(event)
 
-        for message, metadata in logs:
-            s.send(serialize_message(message, metadata))
+        for log_message, log_metadata in logs:
+            s.send(serialize_message(log_message, merge_dicts(metadata, log_metadata)))
     finally:
         s.close()
 
 
 # Handle CloudWatch events and logs
 def awslogs_handler(event):
-    # Get logs
     data = zlib.decompress(base64.b64decode(event['awslogs']['data']), 16 + zlib.MAX_WBITS)
     logs = json.loads(data.decode('utf-8'))
-
-    structured_logs = []
-
-    # Send lines to Logstash
-    for log in logs['logEvents']:
-        structured_logs.append((
-            log['message'], {
-                'aws': {
-                    'log_group': logs['logGroup'],
-                    'log_stream': logs['logStream'],
-                    'owner': logs['owner'],
-                    'log_event_id': log['id'],
-                    'log_timestamp': log['timestamp'],
-                }
+    return [
+        (log['message'], {
+            'aws': {
+                'log_group': logs['logGroup'],
+                'log_stream': logs['logStream'],
+                'owner': logs['owner'],
+                'log_event_id': log['id'],
+                'log_timestamp': log['timestamp'],
             }
-        ))
-
-    return structured_logs
+        }) for log in logs['logEvents']
+    ]
 
 
 def merge_dicts(a, b):
@@ -88,7 +81,7 @@ def merge_dicts(a, b):
                 pass  # same leaf value
             else:
                 raise Exception(
-                        'Conflict while merging metadatas and the log entry at %s' % '.'.join(path + [str(key)]))
+                    'Conflict while merging metadatas and the log entry at %s' % '.'.join(path + [str(key)]))
         else:
             a[key] = b[key]
     return a
