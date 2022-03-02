3ae38ee696af45c6e89f8e8cf363d393d98bfa65
Luboš Mátl <matllubos@gmail.com>
2022-03-02 09:12:50 +0100

    Fixed merge dicts

--- lambda_function.py
+++ lambda_function.py
@@ -72,16 +72,18 @@ def awslogs_handler(event):
     ]
 
 
-def merge_dicts(a, b):
+def merge_dicts(a, b, path=None):
+    path = [] if path is None else path
+
     for key in b:
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge_dicts(a[key], b[key], [str(key)])
+                merge_dicts(a[key], b[key], path + [str(key)])
             elif a[key] == b[key]:
                 pass  # same leaf value
             else:
                 raise Exception(
-                    'Conflict while merging metadatas and the log entry at %s' % '.'.join(path + [str(key)]))
+                        'Conflict while merging metadatas and the log entry at %s' % '.'.join(path + [str(key)]))
         else:
             a[key] = b[key]
     return a
